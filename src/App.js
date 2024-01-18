ac48487301e730173d9b53d1ce33eef9854d7eef
nguyenphuhuy0405 <nguyenphuhuybgg@gmail.com>
2024-01-19 00:32:13 +0700

    "Fix private routes v1"

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Fragment } from 'react'
 import './App.css'
 import { DefaultLayout } from '~/layouts'
 import AdminPrivateRoutes from './routes/AdminPrivateRoutes'
+import PrivateRoutes from './routes/PrivateRoutes'
 
 function App() {
     return (
         <div className="App">
+            {/* Public routes */}
             <Routes>
                 {publicRoutes.map((route, index) => {
                     const Page = route.component
@@ -33,30 +35,33 @@ function App() {
                     )
                 })}
 
-                {/* Admin private routes */}
-                <Route element={<AdminPrivateRoutes />}>
-                    {adminPrivateRoutes.map((route, index) => {
-                        const Page = route.component
+                {/* Private routes */}
+                <Route element={<PrivateRoutes />}>
+                    {/* Admin private routes */}
+                    <Route element={<AdminPrivateRoutes />}>
+                        {adminPrivateRoutes.map((route, index) => {
+                            const Page = route.component
 
-                        let Layout = DefaultLayout
-                        if (route.layout) {
-                            Layout = route.layout
-                        } else if (route.layout === null) {
-                            Layout = Fragment
-                        }
+                            let Layout = DefaultLayout
+                            if (route.layout) {
+                                Layout = route.layout
+                            } else if (route.layout === null) {
+                                Layout = Fragment
+                            }
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        )
-                    })}
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            )
+                        })}
+                    </Route>
                 </Route>
             </Routes>
         </div>

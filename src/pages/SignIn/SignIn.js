d14a90be2668b96f24fd9b50bde02a873ae73bd7
nguyenphuhuy0405 <nguyenphuhuybgg@gmail.com>
2024-05-10 00:51:17 +0700

    "Update SignIn"

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -77,7 +77,7 @@ export default function SignIn() {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Đăng nhập
+                        Đăng nhập ở đây
                     </Typography>
                     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
                         <TextField
